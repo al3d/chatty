159a28b81a48574cefab26e58610839bcc4bdc37
Aled Bartholomew <aled@smotyn.com>
2020-03-31 01:40:01 +0100

    Updated routes definitions

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,18 +1,43 @@
 <?php
 
+use App\Http\Controllers\Auth;
+use App\Support\Url;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('/auth')->group(function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    /**
+     * /csrf-cookie is a route added by the sanctum package.
+     * it's usually under /sanctum prefix, but through a bit
+     * of code research I found you can customise the prefix
+     * in the config/sanctum.php file, so I changed it to be
+     * in line with the rest of these
+     */
+
+    Route::middleware(['guest'])->group(function () {
+        Route::post('/start', Auth\StartController::class);
+        Route::post('/login', Auth\LoginController::class);
+        Route::post('/register', Auth\RegisterController::class);
+        Route::get('/magic-link/{userHash}', Auth\MagicLinkController::class)
+            ->name('magic_link')
+            ->middleware('signed');
+    });
+
+    Route::post('/logout', Auth\LogoutController::class);
+});
+
+/**
+ * Fallback url for any GET requests to our frontend
+ * that don't match. Performs a specific redirect to the frontend.
+ */
+Route::get('/{url}', function ($url) {
+    return redirect(Url::frontend('/' . $url), 301);
+})->where('url', '.*');
+
+/**
+ * Fallback for any other types of request (POST, PUT, DELETE, OPTIONS)
+ * that don't match above. Simply redirects to the frontend's base url.
+ */
+Route::fallback(function () {
+    return redirect(Url::frontend(), 301);
 });
