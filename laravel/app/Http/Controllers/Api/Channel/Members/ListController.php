f4feda3135a39382eec121d52b553cf5d9afe7cc
Aled Bartholomew <aled@smotyn.com>
2020-04-02 22:59:04 +0100

    Added members list controller

---
+++ laravel/app/Http/Controllers/Api/Channel/Members/ListController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Channel\Members;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Resources\UserResource;
+use App\Models\Channel;
+use Illuminate\Http\Request;
+
+class ListController extends BaseController
+{
+
+    public function __invoke(Request $request, Channel $channel)
+    {
+        $collection = $channel
+            ->members()
+            ->get()
+        ;
+
+        return UserResource::collection($collection)
+            ->additional([
+                'channel' => $channel->name,
+            ])
+        ;
+    }
+}
