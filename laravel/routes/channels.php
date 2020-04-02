8356c7c625e78e87dbe0c71444bc12369f03a331
Aled Bartholomew <aled@smotyn.com>
2020-04-02 22:57:27 +0100

    Update channels routes

--- laravel/routes/channels.php
+++ laravel/routes/channels.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
-
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('channel.{name]', function (User $user, $name) {
+    /**
+     * We've defined this as a private channel simply in order to
+     * ensure that users need to be authenticated.
+     *
+     * If we were to add additional security and authorization features
+     * this would be important.
+     */
+    return true;
 });
+
+// Broadcast::channel('App.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
