d9415af8ae472f1508c12c378cccf6a35fcb09b5
Aled Bartholomew <aled@smotyn.com>
2020-04-02 22:58:29 +0100

    Added channels api routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,4 +6,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/me', Api\MeController::class);
+
+    Route::post('/channels', Api\Channel\CreateController::class);
+    Route::get('/channels/{channel}', Api\Channel\ShowController::class);
+    Route::patch('/channels/{channel}', Api\Channel\UpdateController::class);
+    Route::patch('/channels/{channel}/leave', Api\Channel\LeaveController::class);
+    Route::delete('/channels/{channel}', Api\Channel\DeleteController::class);
+
+    Route::get('/channels/{channel}/members', Api\Channel\Members\ListController::class);
+
+    Route::get('/channels/{channel}/messages', Api\Channel\Message\ListController::class);
+    Route::post('/channels/{channel}/messages', Api\Channel\Message\CreateController::class);
+
+    Route::get('/channels/{channel}/messages/{message}', Api\Channel\Message\ShowController::class);
+    Route::patch('/channels/{channel}/messages/{message}', Api\Channel\Message\UpdateController::class);
+    Route::delete('/channels/{channel}/messages/{message}', Api\Channel\Message\DeleteController::class);
+
 });
