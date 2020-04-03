b1f10a3fd931c15cefc1acd8bd1518148b8f6640
Aled Bartholomew <aled@smotyn.com>
2020-04-03 20:44:37 +0100

    added laravel-websockets:
    - added support for it as 'localsockets', so we don't conflict with pusher. Behind the scenes they may use the same settings but it makes it easier to switch for debugging & dev/live environments
    - updated config to look for LOCALSOCKETS_ env variables - a little better/shorter than LARAVEL_WEBSOCKETS
    - docker-composer container uses the same laravel code but only runs the laravel-websockets artisan process, not all the others. A clever way of splitting things out without getting involved with nginx and proxies (in theory)

--- laravel/config/broadcasting.php
+++ laravel/config/broadcasting.php
@@ -11,7 +11,7 @@ return [
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
     |
-    | Supported: "pusher", "redis", "log", "null"
+    | Supported: "pusher", "redis", "log", "null", "localsockets"
     |
     */
 
@@ -38,6 +38,28 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+            ],
+        ],
+
+        /**
+         * This is for laravel-websockets. It closely follows
+         * the same structure as pusher because it's a drop-in
+         * replacement for pusher, but I created it as a new
+         * connection in order to be able to quickly switch
+         * default connections without changing configuration.
+         */
+        'localsockets' => [
+            'driver' => 'pusher',
+            'key' => env('LOCALSOCKETS_APP_KEY'),
+            'secret' => env('LOCALSOCKETS_APP_SECRET'),
+            'app_id' => env('LOCALSOCKETS_APP_ID'),
+            'options' => [
+                'cluster' => env('LOCALSOCKETS_APP_CLUSTER'),
+                'encrypted' => env('LOCALSOCKETS_ENCRYPTED', 'true') === 'true',
+                'useTLS' => env('LOCALSOCKETS_ENCRYPTED', 'true') === 'true',
+                'host' => env('LOCALSOCKETS_HOST', '127.0.0.1'),
+                'port' => env('LOCALSOCKETS_PORT', 6001),
+                'scheme' => env('LOCALSOCKETS_SCHEME', 'http'),
             ],
         ],
 
