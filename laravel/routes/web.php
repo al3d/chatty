aaa6f1e9280a59696e19d4218e0b10b4b724c28d
Aled Bartholomew <aled@smotyn.com>
2020-04-03 20:28:57 +0100

    updated web routes:
    - removed middleware - adding it explicitly in in controller now for better code separation
    - removed grouping
    - added ping endpoint
    - fixed fallback endpoint - correct frontend url now

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,26 +5,24 @@ use App\Support\Url;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Response;
 
-Route::prefix('/auth')->group(function () {
+/**
+ * /csrf-cookie is a route added by the sanctum package.
+ * it's usually under /sanctum prefix, but through a bit
+ * of code research I found you can customise the prefix
+ * in the config/sanctum.php file, so I changed it to be
+ * in line with the rest of these
+ */
+Route::post('/auth/start', Auth\StartController::class);
+Route::post('/auth/login', Auth\LoginController::class);
+Route::post('/auth/logout', Auth\LogoutController::class);
+Route::post('/auth/register', Auth\RegisterController::class);
+Route::get('/auth/magic-link/{user}', Auth\MagicLinkController::class)
+    ->name('magic_link');
 
-    /**
-     * /csrf-cookie is a route added by the sanctum package.
-     * it's usually under /sanctum prefix, but through a bit
-     * of code research I found you can customise the prefix
-     * in the config/sanctum.php file, so I changed it to be
-     * in line with the rest of these
-     */
-
-    Route::middleware(['guest'])->group(function () {
-        Route::post('/start', Auth\StartController::class);
-        Route::post('/login', Auth\LoginController::class);
-        Route::post('/register', Auth\RegisterController::class);
-        Route::get('/magic-link/{user}', Auth\MagicLinkController::class)
-            ->name('magic_link')
-            ->middleware('signed');
-    });
-
-    Route::post('/logout', Auth\LogoutController::class);
+Route::get('/ping', function () {
+    return Response::json([
+        'ack' => time(),
+    ]);
 });
 
 /**
@@ -33,5 +31,5 @@ Route::prefix('/auth')->group(function () {
  * zeit offer, we might run into infinity redirects for api urls.
  */
 Route::any('/{url}', function ($url = '') {
-    return Response::redirectTo(Url::frontend($url), 301);
+    return Response::redirectTo(Url::frontend('/' . $url), 301);
 })->where('url', '.*');
