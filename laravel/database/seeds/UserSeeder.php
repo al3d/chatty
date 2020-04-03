735168876a29db419fd713fea5e8ee1868400136
Aled Bartholomew <aled@smotyn.com>
2020-04-03 20:33:19 +0100

    updated user seeder to add more random users

--- laravel/database/seeds/UserSeeder.php
+++ laravel/database/seeds/UserSeeder.php
@@ -26,5 +26,7 @@ class UserSeeder extends Seeder
             'email' => 'jane@example.org',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
+
+        factory(User::class, 5)->create();
     }
 }
